e5b0074a82abb2b5c401a6c28b305f7aa239e218
Cr28self <tkhoon3study@gmail.com>
2022-09-23 23:36:54 +0900

    Chapter3 DateHead까지

--- App.js
+++ App.js
@@ -10,46 +10,20 @@ import {
     useColorScheme,
     View,
 } from 'react-native';
+import DateHead from "./src/Components/DateHead";
 
-import LearnBtn from './src/Components/LearnBtn';
-import Box from "./src/Components/Box";
-import Counter from "./src/Components/Counter";
 
 
 const App = () => {
 
-    const [visible, setVisible] = useState(true)
-    const [count, setCounter] = useState(0)
-
-    const onPress =()=>{
-        setVisible(!visible)
-    }
-
-    const onIncreaseCounter =()=>{
-        setCounter(count + 1)
-    }
-    const onDecreaseCounter =()=>{
-        setCounter(count - 1)
-    }
-    return (
-        <SafeAreaView style={styles.full}>
-            <View>
-                <LearnBtn onPress={onPress}/>
-                {visible && <Box rounded size="large" color="pink"/>}
-
-
-            </View>
-            <Counter count={count} onIncrease={onIncreaseCounter} onDecrease={onDecreaseCounter}/>
+    const today = new Date();
+    console.log(today);
+    return(
+        <SafeAreaView>
+            <DateHead date={today}/>
         </SafeAreaView>
-    );
-};
 
-const styles = StyleSheet.create({
-    full: {
-        flex: 1,
-
-    },
-})
-
+    )
+}
 
 export default App;
